419f359c68119008de0b8f927adacf3be5af700f
Hisham Muhammad <hisham@gobolinux.org>
2018-07-27 13:54:52 -0300

    wip

--- kong/db/strategies/cassandra/plugins.lua
+++ kong/db/strategies/cassandra/plugins.lua
@@ -2,36 +2,23 @@ local cassandra = require "cassandra"
 
 
 local fmt = string.format
+local null = ngx.null
+local concat = table.concat
+local insert = table.insert
 
 
 local Plugins = {}
 
 
-local function convert_input(value, typ)
-  return (value == nil or value == ngx.null)
-         and cassandra.null
-         or cassandra[typ](value)
-end
-
-
 local function convert_foreign(row, field, id_field)
   local id = row[id_field]
-  if id == nil or id == ngx.null then
-    row[field] = ngx.null
+  if id == nil or id == null then
+    row[field] = null
   else
     row[field] = { id = id }
   end
   row[id_field] = nil
 end
-
-
-local select_q = "SELECT * FROM plugins " ..
-                 " WHERE name = ?" ..
-                 " AND route_id = ?" ..
-                 " AND service_id = ?" ..
-                 " AND consumer_id = ?" ..
-                 " AND api_id = ?" ..
-                 " ALLOW FILTERING"
 
 
 function Plugins:select_by_ids(name, route_id, service_id, consumer_id, api_id)
@@ -42,13 +29,32 @@ function Plugins:select_by_ids(name, route_id, service_id, consumer_id, api_id)
   local res   = {}
   local count = 0
 
-  local args = {
-    convert_input(name, "text"),
-    convert_input(route_id, "uuid"),
-    convert_input(service_id, "uuid"),
-    convert_input(consumer_id, "uuid"),
-    convert_input(api_id, "uuid"),
-  }
+  local exp = {}
+  local args = {}
+  if name ~= nil and name ~= null then
+    insert(exp, "name = ?")
+    insert(args, cassandra.text(name))
+  end
+  if route_id ~= nil and route_id ~= null then
+    insert(exp, "route_id = ?")
+    insert(args, cassandra.uuid(route_id))
+  end
+  if service_id ~= nil and service_id ~= null then
+    insert(exp, "service_id = ?")
+    insert(args, cassandra.uuid(service_id))
+  end
+  if consumer_id ~= nil and consumer_id ~= null then
+    insert(exp, "consumer_id = ?")
+    insert(args, cassandra.uuid(consumer_id))
+  end
+  if api_id ~= nil and api_id ~= null then
+    insert(exp, "api_id = ?")
+    insert(args, cassandra.uuid(api_id))
+  end
+
+  local select_q = "SELECT * FROM plugins WHERE " ..
+                   concat(exp, " AND ") ..
+                   " ALLOW FILTERING"
   for rows, err in cluster:iterate(select_q, args) do
     if err then
       return nil,
